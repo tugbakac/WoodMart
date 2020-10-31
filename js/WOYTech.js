6c264601a907498ee3ae917544c54bf9f5486964
tugbakac <tugbakac45@gmail.com>
2020-11-01 01:26:07 +0300

    first commit

---
+++ js/WOYTech.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function() {
+  
+    var btn = $('#button');
+
+    $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+          btn.addClass('show');
+    } else {
+          btn.removeClass('show');
+    }
+    });
+
+    btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+    });
+
+    });
+
+
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+        }
